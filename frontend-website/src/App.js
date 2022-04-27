f3e1223aa83c6c98e2692872341050a93025f1a2
pk.22092000 <pk.22092000@gmail.com>
2022-04-27 19:27:38 +0700

    .

--- frontend-website/src/App.js
+++ frontend-website/src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css';
-import UsersList from './components/UsersList'
+import UsersList from './componets/UsersList'
 import {BrowserRouter as Link} from 'react-router-dom'
+import {Button, Col, Container, Modal, Row} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 
   
 var count = 0;
@@ -11,6 +13,8 @@ function App() {
   const [users, setUsers] = useState([]);
   const [usersSort, setUsersSort] = useState([])
   const [gender, setGender] = useState("")
+
+  const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
     axios.get("https://randomuser.me/api?results=10")
@@ -127,6 +131,14 @@ function App() {
                 gender={gender}
               />
         </table>
+
+        {/* Modal */}
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Launch modal with grid
+        </Button>
+
+        <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+        
       </main>
       <footer>
 
@@ -136,3 +148,43 @@ function App() {
 }
 
 export default App;
+
+function MydModalWithGrid(props) {
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Using Grid in Modal
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="show-grid">
+        <Container>
+          <Row>
+            <Col xs={12} md={8}>
+              .col-xs-12 .col-md-8
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
